9632a35132f7a24875c0bdaf4baf51aa734d1978
KerstinGreifensteiner <kerstin.greifensteiner@gmx.at>
2021-12-08 11:01:10 +0100

    Added tests for cost creation, added maximum and minimum value variables in cost class

--- domain_objects/cost.py
+++ domain_objects/cost.py
@@ -15,22 +15,26 @@ class Cost:
 
 	__create_key = object()
 	# Max value is set to (10^9)-1, since the `Cost` object is defined to handle 6 integer digits and 2 decimal digits
-	__max_value = (10**9)-1
+	__max_value_all = (10 ** 9) - 1
+	__max_value_euros = __max_value_all // 100
+	__max_value_cents = 99
+	__min_value_euros = 0
+	__min_value_cents = 0
 	# Pattern used for parsing
 	__parse_pattern = re.compile(r'(?P<euro>:\d{0,6})\.(?P<cents>:\d{2})')
 
 	def __post_init__(self, create_key):
 		validate('create_key', create_key, equals=self.__create_key)
 		validate_dataclass(self)
-		validate('value_in_cents', self.value_in_cents, min_value=0, max_value=Cost.__max_value)
+		validate('value_in_cents', self.value_in_cents, min_value=0, max_value=Cost.__max_value_all)
 	
 	def __str__(self) -> str:
 		return f'{self.value_in_cents // 100}.{self.value_in_cents%100:02}'
 
 	@staticmethod
 	def create(euro: int, cents: int=0) -> 'Cost':
-		validate('euro', euro, min_value=0, max_value=Cost.__max_value // 100)
-		validate('cents', cents, min_value=0, max_value=99)
+		validate('euro', euro, min_value=Cost.__min_value_euros, max_value=Cost.__max_value_euros)
+		validate('cents', cents, min_value=Cost.__min_value_cents, max_value=Cost.__max_value_cents)
 		return Cost((euro*100) + cents, Cost.__create_key)
 
 	@staticmethod
--- tests/domain_objects/test_cost.py
+++ tests/domain_objects/test_cost.py
@@ -1,5 +1,50 @@
+import pytest
+from valid8 import ValidationError
+
 from domain_objects.cost import Cost
 
 
-def test_case():
-	pass
+def test_create_cost_maximum_euros():
+	c = Cost.create((10**7)-1, 5)
+	assert c.euro == (10**7)-1
+
+
+def test_create_cost_maximum_euros_fail():
+	with pytest.raises(ValidationError):
+		Cost.create((10**7) + 1, 5)
+
+
+def test_create_cost_minimum_euros():
+	c = Cost.create(0, 5)
+	assert c.euro == 0
+
+
+def test_create_cost_minimum_euros_fail():
+	with pytest.raises(ValidationError):
+		Cost.create(-1, 5)
+
+
+def test_create_cost_maximum_cents():
+	c = Cost.create(5, 99)
+	assert c.cents == 99
+
+
+def test_create_cost_maximum_cents_fail():
+	with pytest.raises(ValidationError):
+		Cost.create(5, 100)
+
+
+def test_create_cost_minimum_cents():
+	c = Cost.create(5, 0)
+	assert c.cents == 0
+
+
+def test_create_cost_minimum_cents_fail():
+	with pytest.raises(ValidationError):
+		Cost.create(5, -1)
+
+
+def test_create_cost_value_in_cents():
+	c = Cost.create(100, 50)
+	assert c.value_in_cents == 10050
+
