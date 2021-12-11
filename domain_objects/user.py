5808b727ccc7e50d2af5ef6b5b20eb4e6a116956
KerstinGreifensteiner <kerstin.greifensteiner@gmx.at>
2021-12-11 16:54:33 +0100

    Minor fixes for menu and user

--- domain_objects/user.py
+++ domain_objects/user.py
@@ -16,10 +16,6 @@ class Student(User):
 	def book_lesson(self):
 		pass
 
-class Teacher(User):
-	def book_lesson():
-		pass
-
 
 class Admin(User):
 	# TODO: What if the admin is redirected to the django admin page?
