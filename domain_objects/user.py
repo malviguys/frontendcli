4df3e9f22b08a940b1e8abbee54600c6f929a24f
KerstinGreifensteiner <kerstin.greifensteiner@gmx.at>
2021-12-13 21:05:52 +0100

    Added tests for user

--- domain_objects/user.py
+++ domain_objects/user.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-import requests
 from typeguard import typechecked
 from valid8.entry_points_inline import validate
 
@@ -18,7 +17,8 @@ class Username:
         # The pattern will ensure that the username is between 3 and 20 chars long and does not contain underscores
         # and dot nor consequently nor at both ends
         # See https://stackoverflow.com/a/12019115/10301322 for more details
-        validate('Username.value', self.value, min_len=3, max_len=20, custom=pattern(r'^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$'))
+        validate('Username.value', self.value, min_len=3, max_len=20,
+                 custom=pattern(r'^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$'))
 
     def __str__(self) -> str:
         return self.value
@@ -27,17 +27,18 @@ class Username:
 @typechecked
 @dataclass(frozen=True)
 class User:
-    is_admin: bool
     username: Username
     token: str
+    is_admin: bool = field(default=False)
 
-    @property
-    def username(self) -> str:
-        return str(self.username)
-    
-    @property
-    def token(self) -> str:
-        return self.token
+    def __post_init__(self):
+        validate_dataclass(self)
+
+    @staticmethod
+    def create(username: str, token: str, is_admin: bool = False) -> 'User':
+        # TODO: add token string validation
+        return User(Username(username), token, is_admin)
+
 
 # TODO: What to define in the subclasses?
 
